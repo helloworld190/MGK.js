3489a99cd351189170f3d0a82dc1fc1d9daa11ba
Jaam <saamkarimi@jozefschool-vlissingen.nl>
2025-06-09 15:52:19 +0200

    Create EventEmitter.js

---
+++ EventEmitter.js
@@ -0,0 +1,22 @@
+export class EventEmitter {
+  constructor() {
+    this.listeners = {};
+  }
+
+  on(event, callback) {
+    if (!this.listeners[event]) this.listeners[event] = [];
+    this.listeners[event].push(callback);
+  }
+
+  off(event, callback) {
+    if (!this.listeners[event]) return;
+    this.listeners[event] = this.listeners[event].filter((cb) => cb !== callback);
+  }
+
+  emit(event, ...args) {
+    if (!this.listeners[event]) return;
+    for (const cb of this.listeners[event]) {
+      cb(...args);
+    }
+  }
+}
